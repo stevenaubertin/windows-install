12a705ab1a70210d02b25330101f665c7daca44b
Steven Aubertin <stevenaubertin@gmail.com>
2019-02-17 17:15:03 -0500

    Update install.ps1
    + Change torrent client
    + Remove docker
    + Change vscode package
    + Make section

--- install.ps1
+++ install.ps1
@@ -10,29 +10,43 @@ function CreateDirectory([string]$directorypath, [switch]$verbose){
 }
 CreateDirectory("$env:USERPROFILE/repo")
 
-# 
+# Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install programs
 choco upgrade chocolatey
 
-choco install jre8
-choco install 7zip.install
+# IDE
+choco install vscode
+choco install visualstudio2017enterprise
+
+# Browser
 choco install googlechrome
 choco install brave
+
+# Dependencies
+choco install jre8
+
+# Media
+choco install plexmediaserver
 choco install vlc
 choco install spotify
-choco install visualstudiocode
-choco install visualstudio2017enterprise
-choco install greenshot
+
+# Gaming
 choco install steam
+choco install battle.net
+
+# Utils
+choco install greenshot
 choco install dropbox
 choco install googledrive
-choco install docker
+choco install 7zip.install
+
+# Communication
 choco install slack
-choco install battle.net
-choco install webtorrent-desktop
-choco install plexmediaserver
+
+# Torrent
+choco install transmission
 
 # Vagrant
 choco install vagrant
